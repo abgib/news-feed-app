8809365bf56de8596f226c03d463f53efd20aeb1
carl+steve <carl+steve@aa.io>
2015-07-09 15:43:41 -0400

    Add zombie view removal functionality.

--- app/assets/javascripts/views/feeds/index.js
+++ app/assets/javascripts/views/feeds/index.js
@@ -1,6 +1,6 @@
 NewsReader.Views.IndexView = Backbone.View.extend({
   initialize: function(){
-    this.listenTo(this.collection, "sync", this.render)
+    this.listenTo(this.collection, "sync", this.render);
   },
 
   template: JST["feeds/index"],
@@ -10,9 +10,29 @@ NewsReader.Views.IndexView = Backbone.View.extend({
 
     this.collection.each(function(feed){
       var indexItemView = new NewsReader.Views.FeedListItemView({model: feed});
+      this.subViews().push(indexItemView);
       this.$(".feed-list").append(indexItemView.render().$el);
     }.bind(this))
 
     return this;
+  },
+
+  remove: function() {
+    if (this.subViews().length > 0) {
+      this.subViews().forEach(function(subView) {
+        subView.remove()
+      });
+    }
+
+    this.stopListening();
+  },
+
+  subViews: function() {
+    if (this._subViews) {
+      return this._subViews;
+    }
+
+    this._subViews = [];
+    return this._subViews;
   }
 })
